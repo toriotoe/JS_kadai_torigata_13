9a7a5ef40616672937e6c88a7d2d873c78fddc66
HOMEPC\音恵 <toriotox.1993@outlook.jp>
2018-03-22 00:10:41 +0900

    先程あげたものがうまく表示されないようなのでもう一度再掲します。

--- Javapractice/src/object/ObjectPractice03.java
+++
@@ -1,9 +0,0 @@
-package object;
-
-public class ObjectPractice03 {
-    public static void main(String[] args) {
-        Circle third = new Circle();
-        Circle.radius =20;
-        third.anSwer();
-    }
-}
--- Javapractice/src/object/ObjectPractice04.java
+++
@@ -1,8 +0,0 @@
-package object;
-
-public class ObjectPractice04 {
-    public static void main(String[] args) {
-        person AKIKO = new person("明子","青森");
-        AKIKO.introduction();
-    }
-}
